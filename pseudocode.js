3d8a2747e6146f43df089088f9591571392237fb
Ting-Yu Chen <7ingyu@gmail.com>
2022-06-08 19:01:47 -0700

    added pseudocode example

---
+++ pseudocode.js
@@ -0,0 +1,14 @@
+// initial answer
+
+// left
+if (answer === 'left') {
+  answer = window.prompt('You see cats')
+  // stay
+    if (answer === 'stay') {
+
+      // stay
+      // spread the world
+    }
+  // continue
+}
+// right
